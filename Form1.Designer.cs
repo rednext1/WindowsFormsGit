b2773cd357c337df0962d97406b5ca078f0b89b2
Yan <none@none.com>
2023-10-25 21:38:41 +0300

    First commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            button1 = new Button();
             SuspendLayout();
             // 
             // label1
@@ -40,14 +41,26 @@
             label1.TabIndex = 0;
             label1.Text = "Test 1";
             // 
+            // button1
+            // 
+            button1.Location = new Point(335, 359);
+            button1.Name = "button1";
+            button1.Size = new Size(166, 70);
+            button1.TabIndex = 1;
+            button1.Text = "Generate Quots";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Git Hub Test";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -55,5 +68,6 @@
         #endregion
 
         private Label label1;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,33 @@ namespace WindowsFormsGit
 {
     public partial class Form1 : Form
     {
+        List<string> quotes;
+        Random rand;
         public Form1()
         {
             InitializeComponent();
+            rand = new Random();
+        }
+
+        private  void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = quotes[rand.Next(quotes.Count)];
+
+           
+
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+ try
+            {
+                quotes = (await File.ReadAllLinesAsync("quotes.txt")).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
